3ab113c4a6f39af6da3849d73f2dbe8af4b1e4df
Roman Farooq <TheRomanFarooq@gmail.com>
2022-09-16 23:42:12 +0500

    added overlay-navbar package

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
-import "./App.css";
-import Header from "./components/layout/Header";
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
+import WebFont from "webfontloader";
+import "./App.css";
+import Header from "./components/Layout/Header/Header";
 
 function App() {
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      },
+    });
+  }, []);
+
   return (
     <BrowserRouter>
       <Header />
