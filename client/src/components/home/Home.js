4a2f2865acf77506b0d37ff15eabcedaa6505033
Roman Farooq <TheRomanFarooq@gmail.com>
2022-09-19 23:51:13 +0500

    created loader component and added react-toastify

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,19 +1,37 @@
+import { useEffect } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.css";
 import ProductCard from "./ProductCard";
 import MetaData from "../Layout/MetaData";
+import Loader from "../Loader/Loader";
 import { getProducts } from "../../Actions/productAction";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Home() {
-  
   const dispatch = useDispatch();
   const { products, loading, error } = useSelector((state) => state.products);
 
   useEffect(() => {
-    dispatch(getProducts());
-  }, [dispatch]);
+    const notify = () => {
+      toast.error(error, {
+        theme: "dark",
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    };
+    if (error) {
+      notify();
+    } else {
+      dispatch(getProducts());
+    }
+  }, [dispatch, error]);
 
   return (
     <>
@@ -28,12 +46,17 @@ function Home() {
         </a>
       </div>
       <h1 className="homeHeading">Featured Products</h1>
-      <div className="container" id="container">
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product._id} product={product} />
-          ))}
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="container" id="container">
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+        </div>
+      )}
+      <ToastContainer />
     </>
   );
 }
